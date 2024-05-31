622b1d401dc8091568d1d4a40565326d1dc8fec4
Songjh0322 <happymine0322@naver.com>
2024-06-01 00:37:00 +0900

    추가

--- C프로그래밍/HW13-1/HW13-1/HW13-1.c
+++ C프로그래밍/HW13-1/HW13-1/HW13-1.c
@@ -1,46 +1,33 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <math.h>
+
+struct rectangle {
+	int width;
+	int height;
+};
+
 
 struct point {
 	int x;
 	int y;
 };
 
-struct rectangle {
-	struct point bottom_left;
-	struct point top_right;
-};
-
-double calculate_area(struct rectangle rect) {
-	int width = rect.top_right.x - rect.bottom_left.x;
-	int height = rect.top_right.y - rect.bottom_left.y;
-	return (double)(width * height);
-}
-
-double calculate_perimeter(struct rectangle rect) {
-	int width = rect.top_right.x - rect.bottom_left.x;
-	int height = rect.top_right.y - rect.bottom_left.y;
-	return 2.0 * (width + height);
-}
-
 int main(void) {
+
 	struct point p1, p2;
-	struct rectangle rect;
-
-	printf("왼쪽 하단 꼭지점의 좌표를 입력하시오(x y): ");
+	struct rectangle dist;
+	int area, circum;
+	printf("왼쪽 하단의 좌표를 입력하세요: ");
 	scanf("%d %d", &p1.x, &p1.y);
-
-	printf("오른쪽 상단 꼭지점의 좌표를 입력하시오(x y): ");
+	printf("오른쪽 상단의 좌표를 입력하세요: ");
 	scanf("%d %d", &p2.x, &p2.y);
+	dist.width = p2.x - p1.x;
+	dist.height = p2.y - p1.y;
+	area = dist.width * dist.height;
+	circum = dist.width * 2 + dist.height * 2;
 
-	rect.bottom_left = p1;
-	rect.top_right = p2;
+	printf("면적은 %d이고 둘레는 %d입니다.\n", area, circum);
 
-	int area = calculate_area(rect);
-	int perimeter = calculate_perimeter(rect);
-
-	printf("면적은 %d이고 둘레는 %d입니다.\n", area, perimeter);
 
 	return 0;
 }
--- C프로그래밍/HelloWorld/HelloWorld/hello.c
+++ C프로그래밍/HelloWorld/HelloWorld/hello.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 
-void sub(int b[]) {
-	b[0] = 4;
-	
-}
-int main(void)
-{
-	int a[3] = { 1,2,3 };
-	sub(a);
-	for (int i = 0; i < 3; i++) {
-		printf("%d ", a[i]);
-	}
-}
+struct vector {
+	double x;
+	double y;
+};
 
+struct vector get_vector_sum(struct vector a, struct vector b) {
+	struct vector result;
+
+	result.x = a.x + b.x;
+	result.y = a.y + b.y;
+
+	return result;
+
+}
+int main(void) {
+	struct vector v1 = { 2.0,3.0 };
+	struct vector v2 = { 1.0,4.0 };
+	struct vector vectorSum;
+	vectorSum = get_vector_sum(v1, v2);
+	printf("벡터의 합은 (%.f, %.f)입니다.\n", vectorSum.x, vectorSum.y);
+}
